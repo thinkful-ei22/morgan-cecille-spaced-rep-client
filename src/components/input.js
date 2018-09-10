ade05ff66f3ede2a612922e659e41bd7537d1a5a
MorganHuegel <mhue2910@gmail.com>
2018-09-10 12:17:24 -0400

    updated login page to include GIF and brief description

--- src/components/input.js
+++ src/components/input.js
@@ -10,7 +10,7 @@ export default class Input extends React.Component {
     render() {
         let error;
         if (this.props.meta.touched && this.props.meta.error) {
-            error = <div className="form-error">{this.props.meta.error}</div>;
+            error = <span className="form-error">{this.props.meta.error}</span>;
         }
 
         let warning;
