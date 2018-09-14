e3f9678bc4e2890fde643d2c907467618e28e764
MorganHuegel <mhue2910@gmail.com>
2018-09-14 12:37:51 -0400

    styled confirm-reset-levels component

--- src/components/confirm-reset.js
+++ src/components/confirm-reset.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import '../components-css/confirm-reset.css';
+
 import { resetUserLevels } from '../actions/users';
 import { displayGameplay } from '../actions/game';
 
+
 export default function ConfirmReset(props) {
   return(
-    <div>
+    <div className='confirm-reset'>
       <form id='confirm-reset' name='confirm-reset'>
         <p>Are you sure you want to reset your proficiency levels?</p>
-        <p>(This will reset ALL questions back to level 1)</p>
+        <p className='reset-level-warning'>(This will reset ALL questions back to level 1)</p>
 
         <button type='reset' onClick={() => props.toggleConfirmReset(false)}>No, keep my progress</button>
 
         <button type='submit' onClick={ () => {
             props.dispatch(resetUserLevels())
             .then( () => props.dispatch(displayGameplay(false)))
-            .then(() => props.toggleConfirmReset(false))
+            .then(() => props.toggleConfirmReset(false));
           }}>Yes, reset my proficiency</button>
       </form>
     </div>
